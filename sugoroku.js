92fa03f7cfb8c035bc51e92580d7d8b27492ff67
sudo-roa <sudo.roa9393@gmail.com>
2021-08-09 02:12:18 +0900

    update sugoroku

--- sugoroku.js
+++ sugoroku.js
@@ -1,59 +1,95 @@
 var counts=0;
 
-function dice(){
-    var diceText = document.querySelector('#num-textarea');
-    var number = Math.floor(Math.random()*100 % 6 + 1) ;
-    diceText.value = number;
-    count();
-    move(number);
+function sugoroku(){
+    dice();
+    moveFixed(number);
+    move(playerLocation, locationNumber, destinationNumber);
+    
 }
 
+// Dice value
+function dice(){
+    var diceText = document.querySelector('#num-textarea');
+    var disable = document.querySelector('#num-btn');
+    disable.setAttribute("disabled","True");
+    number = Math.floor(Math.random()*100 % 6 + 1) ;
+    diceText.value = number;
+    count();
+    return number;
+}
+
+// Number of dice rolled
 function count(){
     counts++;
     var setcount = document.querySelector('#count');
     setcount.value=counts;
-    console.log(counts);
 }
 
-function move(diceNumber){
-    var player = document.querySelector('.player');
-    var before = document.getElementById(player.id);
-    var masu = before.id.slice(4);
-    var destination = Number(masu) + diceNumber;
-    var after = document.getElementById("masu"+destination);
-    before.setAttribute("class","square");
-    before.removeAttribute("style","background-color:#000000;");
-    walk(diceNumber, i = 0);
-    after.setAttribute("class","square player");
-    after.setAttribute("style","background-color:bisque;");
-
-    if(destination>=49){
-        finish(destination);
+// Determine the squares before and after the move
+function moveFixed(diceNumber){
+    if(counts===1){
+        player_direction = "player_right";
     }
+    var player = document.querySelector("."+player_direction);
+    playerLocation = document.getElementById(player.id);
+    locationNumber = Number(playerLocation.id.slice(4));
+    destinationNumber = locationNumber + diceNumber;
+    return(playerLocation, locationNumber, destinationNumber);
+}
+
+// player movement
+function move(playerLocation, moveStart, moveEnd){
+    beforeMove(playerLocation,moveStart,moveEnd);
+}
+
+function beforeMove(playerLocation,moveStart, moveEnd){
+    setTimeout(function(){
+        playerLocation.setAttribute("class","square");
+        playerLocation.removeAttribute("style","background-color:#000000;");
+        moveStart++;
+        nextNumber = moveStart;
+        nextStep = document.getElementById("masu"+moveStart);
+        afterMove(nextNumber, nextStep, moveEnd);    
+    },100);
+}
+
+function afterMove(nextNumber, nextStep, moveEnd){
+    setTimeout(function(){
+        if(nextNumber===13){
+            player_direction = "player_left";
+        }else if(nextNumber===37){
+            player_direction = "player_right";
+        }
+        if(nextNumber < moveEnd){
+            console.log(nextNumber);
+            nextStep.setAttribute("class","square "+player_direction);
+            nextStep.setAttribute("style","background-color:bisque;");
+            beforeMove(nextStep,nextNumber,moveEnd)
+            if(moveEnd>49){
+                finish(moveEnd);
+            }
+        }else{
+            if(moveEnd>49){
+                finish(moveEnd);
+            }else{
+                nextStep.setAttribute("class","square "+player_direction);
+                nextStep.setAttribute("style","background-color:bisque;");
+                console.log("移動終了");
+                var disable = document.querySelector('#num-btn');
+                disable.removeAttribute("disabled","True");
+                finish(moveEnd);
+            }
+        }
+    },100);
     
 }
 
-function walk(diceNumber, i){
-    
-    if(i < diceNumber){
-        setTimeout(function(){
-            console.log(i);
-            i++;
-            walk(diceNumber,i);
-        },100);
-    }
-    // 非同期処理でうまくいかない
-    // for(var i=1;i<diceNumber;i++){
-    //     var moving = document.getElementById("masu"+i);
-    //     sleep(300);
-    //     moving.setAttribute("class","square player");
-    //     moving.setAttribute("style","background-color:bisque;");
-    //     sleep(300);
-    //     moving.setAttribute("class","square");
-    //     moving.removeAttribute("style","background-color:#000000;");  
-    // }
-}
-
+// end processing
 function finish(finishNumber){
-    console.log(finishNumber);
+    if(finishNumber < 49){
+    }else if(finishNumber===49){
+        alert("ご～～～～る！！！もう一回遊んでね！");
+    }else if(finishNumber>49){
+        alert("歩きすぎたのであなたは海の藻屑になりました");
+    }
 }
